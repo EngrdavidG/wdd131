759f8b7b408f6b6761bff30985e6d43bbb769fb6
engrdavdg <splendourcoders@gmail.com>
2026-04-03 20:00:25 +0100

    add another assignment

---
+++ scripts/form.js
@@ -0,0 +1,22 @@
+// PRODUCT ARRAY
+const products = [
+  { id: "fc-1888", name: "flux capacitor" },
+  { id: "fc-2050", name: "power laces" },
+  { id: "fs-1987", name: "time circuits" },
+  { id: "ac-2000", name: "low voltage reactor" },
+  { id: "jj-1969", name: "warp equalizer" }
+];
+
+// POPULATE SELECT
+const select = document.getElementById("product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
+});
+
+// FOOTER
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
